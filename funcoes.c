bed26a645d2dbe9baf3981d870bae1ac11388596
tonelli05 <pedrotonellidacunha@gmail.com>
2024-11-25 17:43:52 -0300

    exibirLista, liberarLista e pesquisarPaciente feitas

--- funcoes.c
+++ funcoes.c
@@ -31,8 +31,18 @@ void inserirPacienteOrdenado(LISTA* l, PONT novo_paciente)
 
 // Função para pesquisar um paciente pelo nome
 PONT pesquisarPaciente(LISTA* l, char* nome)
-{
+{   
+    PONT atual;
+    atual = l->inicio;
 
+    while(atual != NULL){
+        if(strcmp(atual->reg.nome, nome) == 0){
+            return atual;  // Se for igual a 0, ambos as strings sao iguais, logo, paciente encontrado
+        }
+        atual = atual->prox;
+    }
+
+    return NULL;  // Paciente nao encontrado
 }
 
 // Função para retirar um paciente da lista com base no nome
@@ -50,11 +60,45 @@ void salvarPacientesEmArquivo(LISTA* l, char* nome_arquivo)
 // Função para exibir uma quantidade específica de pacientes ordenados na lista
 void exibirLista(LISTA* l, int quantidade)
 {
+    if (l == NULL || l->inicio == NULL) {  // Verifica se a lista esta vazia
+        printf("A lista esta vazia ou a quantidade solicitada eh invalida.\n");
+        return;
+    }
 
+    if (quantidade <= 0) {  // Garante que "quantidade" seja positivo e maior que 0
+        printf("Quantidade solicitada eh invalida.\n");
+        return;
+    }
+
+    PONT atual;
+    atual = l->inicio;
+    int count = 0;  
+
+    while((atual != NULL) && (count < quantidade)){
+        printf("Posicao: %d \n", atual->reg.posicao);
+        printf("Nome: %s \n", atual->reg.nome);
+        printf("Gravidade: %d \n", atual->reg.gravidade);
+        printf("Horario de Chegada: %s", ctime(&atual->reg.horario_chegada));
+        printf("------------------------\n");  // Facilita visualmente a separacao entre pacientes
+        atual = atual->prox;
+        count++;
+    }
 }
 
 // Função para liberar a memória alocada para a lista de pacientes
 void liberarLista(LISTA* l)
 {
-    
+    if(l == NULL)  // Verifica se a lista esta vazia
+        return;
+
+    PONT atual;
+    atual = l->inicio;
+
+    while(atual != NULL){
+        PONT apagar = atual;
+        atual = atual->prox;
+        free(apagar);
+    }
+
+    l->inicio = NULL;
 }
